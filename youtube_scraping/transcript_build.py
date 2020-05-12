775433b1f8672d568ea5359ba62d230fe4e05512
tyxiang0530 <56275354+tyxiang0530@users.noreply.github.com>
2020-05-12 16:55:07 -0700

    Update transcript_build.py

--- youtube_scraping/transcript_build.py
+++ youtube_scraping/transcript_build.py
@@ -1,3 +1,12 @@
+'''
+FREE GOURDS
+TAI XIANG
+GUY THAMPAKKUL
+SAM MILLETTE
+transcript_build takes in a csv of youtube links and iterates through them, outputting the subtitles to
+a csv file. If the video is age restricted and the transcript cannot be accessed, then an asterisk is outputted
+If a video transcript happens to be in another language, it is automatically translated with google's machine translation library
+'''
 from youtube_transcript_api import YouTubeTranscriptApi
 import nltk
 from nltk.tokenize import word_tokenize
@@ -88,6 +97,66 @@ def write_transcripts(category_num, country_in):
         names_in.append(column[2])
     for links, dates, names in zip(links_in, dates_in, names_in):
         get_transcripts(links, dates, names, country_in, category_num)
+
+# gathers the transcript when given a youtube video id but does so for videos that are not filtered by id
+def get_transcripts_all(video_id, date_pub, names_in, country_in):
+    try:
+        transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+        trans_all = []
+        trans_str = ''
+        try:
+            transcript = transcript_list.find_transcript(['en', 'en-US'])
+            text = transcript.fetch()
+            for t in text:
+                clean = preprocess_text(t.get('text'))
+                trans_all.append(clean)
+            for i in trans_all:
+                for x in i:
+                    trans_str += x + ' '
+        except:
+            try: 
+                transcript = transcript_list.find_transcript([
+                    'de', 'zh_Hans', 'fr', 'zh-Hant', 'es',
+                    'ar', 'hi', 'it', 'ru', 'vi', 'th', 'sv',
+                    'fa', 'pt', 'cs', 'na', 'nl', 'el', 'fil', 'zh-TW'
+                ])
+                translated_transcript = transcript.translate('en')
+                text = translated_transcript.fetch()
+                for t in text:
+                    clean = preprocess_text(t.get('text'))
+                    trans_all.append(clean)
+                for i in trans_all:
+                    for x in i:
+                        trans_str += x + ' '
+            except:
+                trans_str = ''
+#         with open('/home/txaa2019/free_gourds/Youtube Grab/text_files/All/' + country_in + '_all_text.csv', 'a') as csvfile:
+        with open('/home/txaa2019/free_gourds/Youtube Grab/text_files/All/global_all_text.csv', 'a') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([date_pub, names_in, trans_str])
+            print('done writing')
+    except Exception as e: 
+        print(e)
+        with open('/home/txaa2019/free_gourds/Youtube Grab/text_files/All/' + country_in + '_all_text.csv', 'a') as csvfile:
+#         with open('/home/txaa2019/free_gourds/Youtube Grab/text_files/All/global_all_text.csv', 'a') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow([date_pub, names_in, '*'])
+            print('done writing')
+	
+# writes the transcript to a csv file
+def write_transcripts_all(country_in):
+    file = open('/home/txaa2019/free_gourds/Youtube Grab/Categories/All/' + country_in + '_nocat.csv')
+#     file = open('/home/txaa2019/free_gourds/Youtube Grab/Categories/All/global.csv')
+    csv_f = csv.reader(file)
+    links_in = []
+    dates_in = []
+    names_in = []
+    for column in csv_f:
+        links_in.append(column[1][32:])
+        dates_in.append(column[0])
+        names_in.append(column[2])
+    for links, dates, names in zip(links_in, dates_in, names_in):
+        get_transcripts_all(links, dates, names, country_in)
 	
 # loop through the categories and countries and write the transcripts to a csv file
 arr_category_num = ['10', '19', '22', '24', '25', '26', '27', '28']
